bd13cf3a49c43834273f7e31600752df984a3f19
xb <ssbandjl@163.com>
2025-05-23 11:08:29 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 # s114:
 # NVIDIA Tesla V100-SXM2
-# make -j src.build CXXFLAGS="-g -O0" DEBUG=1 CUDA_HOME=/usr/lib/cuda NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
-make -j src.build CXXFLAGS="-I/root/project/ai/nccl/src/include -g1" DEBUG=1 CUDA_HOME=/usr/lib/cuda NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
+# make -j src.build  DEBUG=1 CUDA_HOME=/usr/lib/cuda NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
+make -j src.build \
+  BUILDDIR=/root/project/ai/nccl/build \
+  CXXFLAGS="-I/root/project/ai/nccl/src/include -g -O0 -fPIC" \
+  DEBUG=1 \
+  CUDA_HOME=/usr/lib/cuda \
+  NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
 
 # export LD_LIBRARY=/root/project/ai/nccl/build/lib
+
+
+# make -j src.build \
+#   DEBUG=1 \
+#   CXXFLAGS="-g -O0 -fPIC" \
+#   NVCCFLAGS="-g -Xcompiler -fPIC" \
+#   NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
