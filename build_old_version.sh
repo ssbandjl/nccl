356deb6afcad41d6164d067e207aa6b053f7c3b3
xb <bin@sidynamic.com>
2025-06-19 14:26:09 +0800

    u

---
+++ build_old_version.sh
@@ -0,0 +1,21 @@
+# s114:
+# NVIDIA Tesla V100-SXM2
+# make -j src.build  DEBUG=1 CUDA_HOME=/usr/lib/cuda NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
+make -j src.build \
+  BUILDDIR=/root/project/ai/nccl-tests/nccl/build \
+  CXXFLAGS="-I/root/project/ai/nccl-tests/nccl/src/include -g -O0 -fPIC" \
+  DEBUG=1 \
+  ENABLE_TRACE=1 \
+  CUDA_HOME=/usr/lib/cuda \
+  NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
+
+# export LD_LIBRARY=/root/project/ai/nccl-tests/nccl/build/lib
+
+
+# make -j src.build \
+#   DEBUG=1 \
+#   CXXFLAGS="-g -O0 -fPIC" \
+#   NVCCFLAGS="-g -Xcompiler -fPIC" \
+#   NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
+
+
