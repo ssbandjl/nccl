ecff0ce33543803e98cf2a99189c00470598a52c
xb <bin@chinachina.cn>
2025-06-28 15:46:25 +0800

    u

--- src/transport/net_ib.cc
+++ src/transport/net_ib.cc
@@ -1198,7 +1198,7 @@ ncclResult_t ncclIbRtsQp(struct ibv_qp* qp) {
   qpAttr.rnr_retry = 7;
   qpAttr.sq_psn = 0;
   qpAttr.max_rd_atomic = 1;
-  printf_log("qp:%d, qpAttr.timeout:%d, qpAttr.retry_cnt:%d\n", qp->qp_num, qpAttr.timeout, qpAttr.retry_cnt);
+  // printf_log("qp:%d, qpAttr.timeout:%d, qpAttr.retry_cnt:%d\n", qp->qp_num, qpAttr.timeout, qpAttr.retry_cnt);
   NCCLCHECK(wrap_ibv_modify_qp(qp, &qpAttr, IBV_QP_STATE | IBV_QP_TIMEOUT | IBV_QP_RETRY_CNT | IBV_QP_RNR_RETRY | IBV_QP_SQ_PSN | IBV_QP_MAX_QP_RD_ATOMIC));
   return ncclSuccess;
 }
