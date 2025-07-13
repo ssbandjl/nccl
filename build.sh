ded0d4d0ffd9cf4d475308ab7c44868b300bf5e2
xb <bin@chinachina.cn>
2025-07-13 09:19:01 +0800

    u

--- build.sh
+++ build.sh
@@ -11,7 +11,24 @@
 #   CUDA_HOME=/usr/lib/cuda \
 #   NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
 
+# v100 no debug
+# make -j src.build \
+  BUILDDIR=/root/project/ai/nccl-tests/nccl/build \
+  CXXFLAGS="-I/root/project/ai/nccl-tests/nccl/src/include -fPIC" \
+  DEBUG=1 \
+  CUDA_HOME=/usr/lib/cuda \
+  NVCC_GENCODE="-gencode=arch=compute_70,code=sm_70"
+
 # a6000
+# make -j src.build \
+#   BUILDDIR=/root/project/ai/nccl-tests/nccl/build \
+#   CXXFLAGS="-I/root/project/ai/nccl-tests/nccl/src/include -g -Og -fPIC" \
+#   DEBUG=1 \
+#   ENABLE_TRACE=1 \
+#   CUDA_HOME=/usr/lib/cuda \
+#   NVCC_GENCODE="-gencode=arch=compute_86,code=sm_86"
+
+# slow, no 
 # make -j src.build \
 #   BUILDDIR=/root/project/ai/nccl-tests/nccl/build \
 #   CXXFLAGS="-I/root/project/ai/nccl-tests/nccl/src/include -g -O0 -fPIC" \
@@ -21,11 +38,11 @@
 #   NVCC_GENCODE="-gencode=arch=compute_86,code=sm_86"
 
 # a6000 no debug
-make -j src.build \
-  BUILDDIR=/root/project/ai/nccl-tests/nccl/build \
-  CXXFLAGS="-I/root/project/ai/nccl-tests/nccl/src/include -fPIC" \
-  CUDA_HOME=/usr/lib/cuda \
-  NVCC_GENCODE="-gencode=arch=compute_86,code=sm_86"
+# make -j src.build \
+#   BUILDDIR=/root/project/ai/nccl-tests/nccl/build \
+#   CXXFLAGS="-I/root/project/ai/nccl-tests/nccl/src/include -fPIC" \
+#   CUDA_HOME=/usr/lib/cuda \
+#   NVCC_GENCODE="-gencode=arch=compute_86,code=sm_86"
 
 # export LD_LIBRARY=/root/project/ai/nccl-tests/nccl/build/lib
 
